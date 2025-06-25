643d6e875df8a8bb218f4af68c2f37c16bcb2977
Victor <victor.morales@etu.univ-grenoble-alpes.fr>
2025-06-25 11:45:26 +0200

    test pre-commit 6

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -2,6 +2,7 @@
 import { http, HttpResponse } from 'msw';
 import { generateProducts } from '../utils/generateProducts.js';
 
+// comment: This file defines the mock API handlers for the product catalogue.
 export const handlers = [
   http.get('/api/products', () => {
     console.log('Handling GET /api/products request');
